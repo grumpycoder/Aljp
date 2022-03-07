58dff837e5d680555e30c2ea0d19768627b81a8d
Mark Lawrence <marklawrence72@gmail.com>
2022-03-07 16:03:29 -0600

    initial stub for controllers

---
+++ Aljp.Web/Controllers/Api/VendorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aljp.Web.Controllers.Api;
+
+[ApiController]
+[Route("api/[controller]", Name = "Vendors")]
+public class VendorsController: ControllerBase
+{
+    
+    [Route("")]
+    public async Task<ActionResult> Get(CancellationToken token = new())
+    {
+        return Ok();
+    }
+
+}
